039233b13b78fe1f5ba3bd7d351c492c1d8e67d2
fdshzgdfsewagfdhb <ht3826@gmail.com>
2026-09-19 13:20:07 -0700

    sep19

---
+++ nextjs-blog/pages/index.js
@@ -0,0 +1,45 @@
+// wall of imports
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts-json';
+import Link from 'next/link';
+import Date from '../components/date';
+
+// when this file is used the function Home is called 
+export default function Home({ allPostsData }) {
+// this is that is displayed on the home page of the website
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello, I'm Wyatt, I like computers, plants, and telling stories</p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
+// this gets the data that homes needs, i think
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
---
+++ nextjs-blog/pages/posts/[id].js
@@ -0,0 +1,45 @@
+//wall of imports
+import Layout from '../../components/layout';
+import { getAllPostIds, getPostData } from '../../lib/posts-json';
+import Head from 'next/head';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
+
+// when this file is used the function Post is called
+export default function Post({ postData }) {
+// this is how the post page is displayed on the website
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+
+//this is how the post pages know where they are?
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+// this is how that data gotten for the post pages, i think
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+ 
+  return {
+    props: {
+      postData,
+    },
+  };
+}
